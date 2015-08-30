fae3c65f5fb49748323ba1d0b5e726c382e684c4
steven harradine <steven.harradine@telus.com>
2015-08-30 04:58:37 -0400

    send emails if the ip changes and is on the blacklist

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports.DNS_SERVER     = "checkip.dyndns.org"
+module.exports.EMAIL_PROVIDER = 'Gmail'
+module.exports.EMAIL_USER     = 'youremail'
+module.exports.EMAIL_PASSWORD = 'yourpassword'
+module.exports.NOTIFY_EMAIL   = 'youremail'
+module.exports.TEST_INTERVAL  = 3600000   // 1 Hour
+module.exports.IP_BLACKLIST   = [
+  "0.0.0.0",
+  "127.0.0.1"
+]
--- ip-alarm.js
+++ ip-alarm.js
@@ -1,16 +1,93 @@
-var os     = require('os'),
-    ifaces = os.networkInterfaces()
+var CONFIG     = require("./config"),
+    nodemailer = require("nodemailer"),
+    http       = require("http"),
+    old_ip     = ""
 
-getCurrentIP ("eth0", function (ip) {
-  console.log (ip)
-})
+run ()
 
-function getCurrentIP (interface_name, callback) {
-  Object.keys(ifaces).forEach(function (ifname) {
-    ifaces[ifname].forEach(function (iface) {
-      if ('IPv4' == iface.family && ifname == interface_name) {
-        callback (iface.address)
-      }
+function run () {
+  getCurrentIp (CONFIG.DNS_SERVER, function (ip) {
+    if (theIpHasChanged && ipIsInTheBlacklist (ip)) {
+      sendAlerts (ip, function () {
+        console.log ("Starting over")
+
+        setTimeout(run, CONFIG.TEST_INTERVAL)
+      })
+    }
+  })
+}
+
+function sendAlerts (ip, callback) {
+  var transporter = nodemailer.createTransport({
+    service: CONFIG.EMAIL_PROVIDER,
+    auth: {
+      user: CONFIG.EMAIL_USER,
+      pass: CONFIG.EMAIL_PASSWORD
+    }
+  }),
+  email_body = "The IP has changed to " + ip,
+  mailOptions = {                            // setup e-mail data with unicode symbols
+    from: "IP Watch ✔ <ip-watch@alert.com>", // sender address
+    to: CONFIG.NOTIFY_EMAIL,                 // list of receivers
+    subject: "IP drift detected ✘",          // Subject line
+    text: email_body,                        // plaintext body
+    html: email_body                         // html body
+  }
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error){
+      console.log(error);
+    } else {  // message sent
+      //if (verbose) console.log("Message sent: " + info.response)
+      callback()
+    }
+  })
+}
+
+function ipIsInTheBlacklist (ip) {
+  for (i in CONFIG.IP_BLACKLIST) {
+    if (CONFIG.IP_BLACKLIST[i] == ip) {
+      return true
+    }
+  }
+
+  return false
+}
+
+function theIpHasChanged (current_ip) {
+  var ip_has_changed = old_ip != current_ip
+
+  if (ip_has_changed) {
+    console.log (current_ip)
+
+    old_ip = current_ip
+  }
+
+  return ip_has_changed
+}
+
+function getCurrentIp (dns_server, callback) {
+  var options = {
+    host: dns_server,
+    port: 80,
+    path: '/',
+    method: 'POST'
+  }
+
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8')
+    res.on('data', function (chunk) {
+      callback (chunk.split(": ")[1].split("</")[0])
     })
   })
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message)
+  })
+
+  // write data to request body
+  req.write('data\n')
+  req.write('data\n')
+  req.end()
 }
